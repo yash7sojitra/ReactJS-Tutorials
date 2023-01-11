47689d5eea1fa0f686c5cc3c13596fd48ef5f1fb
yash7sojitra <yash.sojitra2002@gmail.com>
2023-01-11 18:52:21 +0530

    Added more Routing functionalities

--- React Router Practice Project/src/App.js
+++ React Router Practice Project/src/App.js
@@ -4,6 +4,7 @@ import Layout from "./components/layout/Layout";
 import AllQuotes from "./pages/AllQuotes";
 import NewQuote from "./pages/NewQuote";
 import QuoteDetail from "./pages/QuoteDetail";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
@@ -20,6 +21,9 @@ function App() {
         </Route>
         <Route path="/new-quote">
           <NewQuote />
+        </Route>
+        <Route path="*">
+          <NotFound />
         </Route>
       </Switch>
     </Layout>
--- React Router Practice Project/src/pages/QuoteDetail.js
+++ React Router Practice Project/src/pages/QuoteDetail.js
@@ -1,14 +1,33 @@
 import { Fragment } from "react";
-import { Route, useParams } from "react-router-dom";
+import { Link, Route, useParams } from "react-router-dom";
 import Comments from "../components/comments/Comments";
+import HighlightedQuote from "../components/quotes/HighlightedQuote";
+
+const DUMMY_QUOTES = [
+  { id: "q1", author: "Max", text: "Learning React is fun" },
+  { id: "q2", author: "Maximilian", text: "Learning React is great" },
+];
 
 const QuoteDetail = () => {
-  const param = useParams();
+  const params = useParams();
+
+  const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
+
+  if (!quote) {
+    return <p>No Quote Found!</p>;
+  }
+
   return (
     <Fragment>
-      <h1>Quote Detail Page </h1>
-      <p>{param.quoteId}</p>
-      <Route path={`/quotes/${param.quoteId}/comments`}>
+      <HighlightedQuote text={quote.text} author={quote.author} />
+      <Route path={`/quotes/${params.quoteId}`} exact>
+        <div className="centered">
+          <Link className="btn--flat" to={`/quotes/${params.quoteId}/comments`}>
+            Load Comments
+          </Link>
+        </div>
+      </Route>
+      <Route path={`/quotes/${params.quoteId}/comments`}>
         <Comments />
       </Route>
     </Fragment>
